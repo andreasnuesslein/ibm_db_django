1bef351aed14d315243638cefaf16a9512b29c11
Andreas Nüßlein <andreas.nuesslein@amnesty.de>
2014-07-22 15:23:03 +0200

    added setup-structures

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='ibm_db_django',
+    version='1.0.6',
+    packages=['ibm_db_django'],
+    url='https://github.com/nutztherookie/ibm_db_django',
+    license='Apache License, Version 2.0',
+    author='Andreas Nüßlein',
+    author_email='andreas.nuesslein@amnesty.de',
+    description=''
+)
+
